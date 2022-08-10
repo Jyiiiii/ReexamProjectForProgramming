a7ccde263e92a60c009eefe3ab3471c195e7db8e
Jyiiiii <jyiiii047@gmail.com>
2022-08-10 21:29:24 +0200

    add bricks

--- js/game.js
+++ js/game.js
@@ -1,19 +1,17 @@
 import Button from "./button.js";
 import Paddle from "./paddle.js";
-import Bricks from "./bricks.js";
+// import Bricks from "./bricks.js";
 
 let state = "start";
 let startButton;
 let restartButton;
 let paddle;
-let brick;
 
 function setup() {
   createCanvas(400, 600);
   startButton = new Button(width - 280, height - 150, 150, 50, 30, "start");
   restartButton = new Button(width - 280, height - 450, 150, 50, 30, "again");
   paddle = new Paddle(width - 350, 500, 150, 30, 20);
-  brick = new Bricks(width - 380, height - 550, 70, 30);
 }
 
 window.setup = setup;
@@ -37,7 +35,23 @@ function gameScreen() {
 
   //   startButton.draw();
   paddle.draw();
-  brick.draw();
+
+  let brickWidth = 65;
+  let margin = 12;
+  let x = margin;
+  let y = 80 + margin;
+  let brickHeight = 30;
+  for (let i = 0; i < 5; i++) {
+    fill(149, 75, 12);
+    rect(x, 50, brickWidth, 30);
+    x = x + brickWidth + margin;
+  }
+
+  for (let i = 0; i < 4; i++) {
+    fill(149, 75, 12);
+    rect(12, y, 65, brickHeight);
+    y = y + brickHeight + margin;
+  }
 }
 
 function resultScreen() {
